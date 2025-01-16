9ad9da480a5a2756872f759431cd353a94769f87
Lucas.LEFEVRE <lucas.lefevre60@gmail.com>
2025-01-16 17:15:06 +0100

    Rules

--- app/index.php
+++ app/index.php
@@ -1,7 +1,19 @@
 <?php
     while (1) {
-        $userInput = menu();
-
+        function rules(){
+            echo "+---------------------------------------------------------------------+\n";
+            echo "|                             REGLES :                                |\n";
+            echo "| Le mode Morpion 3x3 propose un affrontement stratégique en tour par |\n";
+            echo "| tour entre deux joueurs. La grille de jeu est composée de 9 cases,  |\n";
+            echo "| et chaque joueur doit, à son tour, sélectionner une case vide pour  |\n";
+            echo "| y placer son symbole, soit un 'X' soit un 'O'. L'objectif est de    |\n";
+            echo "| former une ligne de trois symboles identiques, que ce soit à        |\n";
+            echo "| à l'horizontale, à la verticale ou en diagonale. Le premier joueur  |\n";
+            echo "| à réaliser cet alignement remporte la partie. En cas de remplissage |\n";
+            echo "| total de la grille sans alignement, le match est déclaré nul.       |\n";
+            echo "+---------------------------------------------------------------------+\n";
+            menu();
+        }
         function menu() : ?string {
             echo "+-----------------------------------------------+";
             echo PHP_EOL;
@@ -21,9 +33,11 @@
             echo PHP_EOL;
             echo "+-----------------------------------------------+";
             echo PHP_EOL;
-            $userChoice = readline("Votre choix : ");
+            $userChoice = strtoupper(readline("Votre choix : "));
             switch ($userChoice) {
                 case "R":
+                    rules();
+                    break;
                 case "J":
                 case "C":
                 case "O":
@@ -35,4 +49,8 @@
             }
             return null;
         }
+
+
+        $userInput = menu();
+
     }
