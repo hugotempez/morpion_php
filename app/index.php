0f1ad146bf91f2b5f0c114ce28867f1febb70a6d
Hugo Tempez <hugotempezpro@gmail.com>
2025-01-16 15:07:05 +0100

    Fin du menu

--- app/index.php
+++ app/index.php
@@ -1,2 +1,38 @@
 <?php
-    echo "Wesh";
+    while (1) {
+        $userInput = menu();
+
+        function menu() : ?string {
+            echo "+-----------------------------------------------+";
+            echo PHP_EOL;
+            echo "| Morpion";
+            echo PHP_EOL;
+            echo "+-----------------------------------------------+";
+            echo PHP_EOL;
+            echo "| R - > Règle du jeu";
+            echo PHP_EOL;
+            echo "| J - > Jeu unique (2 joueurs)";
+            echo PHP_EOL;
+            echo "| C - > Challenge de 3 parties (2 joueurs)";
+            echo PHP_EOL;
+            echo "| O - > Contre l’ordinateur";
+            echo PHP_EOL;
+            echo "| Q - > Quitter";
+            echo PHP_EOL;
+            echo "+-----------------------------------------------+";
+            echo PHP_EOL;
+            $userChoice = readline("Votre choix : ");
+            switch ($userChoice) {
+                case "R":
+                case "J":
+                case "C":
+                case "O":
+                case "Q":
+                    return $userChoice;
+                default:
+                    popen("cls", "w");
+                    menu();
+            }
+            return null;
+        }
+    }
