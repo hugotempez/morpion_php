4d6ba2641aacae11e1c4afbccb1d0a9caa0146a2
PC-FIXE-HUGO\Hugo <hugotempezpro@gmail.com>
2025-01-17 14:59:19 +0100

    HT - squelette objet IA

--- app/IAPlayer.php
+++ app/IAPlayer.php
@@ -2,12 +2,15 @@
 
 include_once 'Player.php';
 
+//TODO: Implémenter les diverses fonctions (toutes privées) qui permettrait à l'IA de jouer
 class IAPlayer extends Player
 {
+    private string $algorithm; //TODO le type d'algo avec lequel l'IA va jouer
 
-    public function __construct(string $name="")
+    public function __construct(string $algo, string $name="")  //TODO passer le type d'algo que l'on veux en paramètre, refuser la création de l'objet si ce n'est pas le cas ou définir un algo par defaut
     {
         parent::__construct();
+        $this->algorithm = $algo;
         $this->id = Player::$counter;
         $this->name = $name;
     }
@@ -15,5 +18,15 @@ class IAPlayer extends Player
     public function __destruct()
     {
         parent::__destruct();
+    }
+
+
+    /**
+     * TODO: En fonction de l'algo choisi et de la map de morpion passé en paramètre, va décider du meilleur coup suivant (le type de retour sera trés probablement à modifier)
+     * @param SplFixedArray $field
+     * @return void
+     */
+    public function play(SplFixedArray $field) : void {
+
     }
 }
